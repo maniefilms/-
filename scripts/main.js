9a11aa3c3cf6cff2becc6acb0d2e7aba9eea3903
maniefilms <maniefilms@gmail.com>
2025-10-02 15:06:03 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
   window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
 
   const FRAME_RATE = 60
-  const PARTICLE_NUM = 2000
+  const PARTICLE_NUM = 3000
   const RADIUS = Math.PI * 2
   const CANVASWIDTH = 500
   const CANVASHEIGHT = 150
